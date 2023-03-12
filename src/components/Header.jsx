9672c95aecc9ab7c80414820ccb5724135330e36
Fernando Rodriguez <ferchulobo2015@gmail.com>
2023-03-12 20:32:19 -0300

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Sound from './Sound'
+import picture from '../assets/img/headerImg.jpg'
+import sound from '../assets/audio/Rick_and_Morty.ogg'
 
 const Header = () => {
   return (
     <section>
     <div className='container_header_img'>
-        <img width="100%" src='/headerImg.jpg' alt="header.jpg" />
-        <Sound src="/public/Rick_and_Morty.ogg"/> 
+        <img width="100%" src={picture} alt="header.jpg" />
+        <Sound src={sound}/> 
     </div>
     </section>
 
